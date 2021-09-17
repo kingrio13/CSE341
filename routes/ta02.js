e73f23651b6379b67987a154f4213d621d1ef598
kingrio13 <kingrio.miranda@gmail.com>
2021-09-17 10:28:26 +0800

    TA02 Activity pushed no Styles

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,58 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+
+const userList= ['rio', 'sam', 'zarry', 'galvez'];
+
+
+router.post('/addUser', (req, res, next) => {
+//console.log(req.body.user);
+const users=req.body.user;
+userList.push(users);
+res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+
+  const users=req.body.user;
+  const checkindex=userList.indexOf(users);
+
+  if(checkindex >= 0){
+      console.log(checkindex);
+      console.log(users);
+      userList.splice(checkindex, 1);
+  }
+  else{
+    console.log('user not found');
+  }
+
+
+  res.redirect('/ta02');
+  });
+  
+  
+
+
+
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
-    title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
+  res.render('pages/ta02', {
+    title: 'Team Activity 02 by',
+    path: 'ta02', // For pug, EJS
+    users:userList,
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
   });
 });
+
+
+
+
+
+
+
+
 
 module.exports = router;
