07c81c64b518bb9a659e399aa2f6762345aded1f
kingrio13 <kingrio.miranda@gmail.com>
2021-09-25 21:56:12 +0800

    mongoDB

--- index.js
+++ index.js
@@ -43,3 +43,36 @@ app
 
 
   app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
+  const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+    origin: "https://cse341projectrio.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://kingrio13:<password>@cluster0.mc5dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+      // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
