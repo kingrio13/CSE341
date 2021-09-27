5cb892e1b68e61a8ed46b2deb6639013c228e148
kingrio13 <kingrio.miranda@gmail.com>
2021-09-27 22:54:22 +0800

    Finalize week2 - lesson 4

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const mongoConnect = require('./util/database').mongoConnect;
+
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
@@ -42,7 +44,7 @@ app
 
 
 
-  //app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 
 
 
@@ -69,6 +71,12 @@ const options = {
     family: 4
 };
 
+//app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+mongoConnect(() => {
+  app.listen(3000);
+});
+
 // const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://kingrio13:mongodb1991@cluster0.mc5dh.mongodb.net/test?retryWrites=true&w=majority";
 
 
@@ -87,15 +95,15 @@ const options = {
 
 
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://kingrio13:mongodb1991@cluster0.mc5dh.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  //console.log(collection);
-  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-  console.log('connected');
-  // perform actions on the collection object
-  client.close();
-});
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://kingrio13:mongodb1991@cluster0.mc5dh.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   //console.log(collection);
+//   app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+//   console.log('connected');
+//   // perform actions on the collection object
+//   client.close();
+// });
 
